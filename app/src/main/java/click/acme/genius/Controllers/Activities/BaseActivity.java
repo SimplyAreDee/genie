ecbedf78cebf7fc9f49d77ffeddf270f66dae925
Lerz <rdelelis1@gmail.com>
2019-02-13 19:10:57 +0100

    Creation BaseActivity + heritage Ajout logique connexion + ecran connexion/creation

---
+++ app/src/main/java/click/acme/genius/Controllers/Activities/BaseActivity.java
@@ -0,0 +1,46 @@
+package click.acme.genius.Controllers.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import click.acme.genius.R;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected abstract int getFragmentLayout();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getFragmentLayout());
+
+        ButterKnife.bind(this);
+    }
+
+    protected OnFailureListener onFailureListener(){
+        return new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    @Nullable
+    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
+
+}
--- app/src/main/java/click/acme/genius/Controllers/Activities/BootstrapActivity.java
+++ app/src/main/java/click/acme/genius/Controllers/Activities/BootstrapActivity.java
@@ -1,34 +1,65 @@
 package click.acme.genius.Controllers.Activities;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Arrays;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import butterknife.BindView;
+import butterknife.OnClick;
 import click.acme.genius.R;
 import click.acme.genius.Utils.ConnectivityCheck;
 
-public class BootstrapActivity extends AppCompatActivity {
+public class BootstrapActivity extends BaseActivity {
 
-    private static final int SPLASH_TIME = 3000;
+    private static final int RC_SIGN_IN = 123;
+
+    @BindView(R.id.bootstrap_activity_refresh_btn) Button mRetryConnectivityButton;
+    @BindView(R.id.bootstrap_activity_login_btn) Button mConnexionButton;
+    @BindView(R.id.bootstrap_activity_coordinator_layout) CoordinatorLayout coordinatorLayout;
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.activity_bootstrap_screen;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bootstrap_screen);
 
+        mRetryConnectivityButton.setVisibility(View.INVISIBLE);
+        mConnexionButton.setVisibility(View.INVISIBLE);
         checkConnectivity();
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    @OnClick(R.id.bootstrap_activity_refresh_btn)
+    void onClickRetryConnectivityButton(View view) {
+        checkConnectivity();
+    }
+
+    @OnClick(R.id.bootstrap_activity_login_btn)
+    void onClickLoginButton(View view){
+        startCreateAccountActivity();
     }
 
     private void checkConnectivity() {
@@ -36,6 +67,7 @@ public class BootstrapActivity extends AppCompatActivity {
             if(internet){
                 startNextActivity();
             }else{
+                mRetryConnectivityButton.setVisibility(View.VISIBLE);
                 showNoConnexionError();
             }
         });
@@ -43,10 +75,13 @@ public class BootstrapActivity extends AppCompatActivity {
 
     private void startNextActivity(){
         //TODO check if account created
-        //If so
+        if(isCurrentUserLogged()) {
+            mConnexionButton.setVisibility(View.INVISIBLE);
             startMainActivity();
-        //else
-            //startCreateAccountActivity();
+        } else {
+            mConnexionButton.setVisibility(View.VISIBLE);
+            startCreateAccountActivity();
+        }
     }
 
     private void startMainActivity(){
@@ -56,7 +91,18 @@ public class BootstrapActivity extends AppCompatActivity {
     }
 
     private void startCreateAccountActivity(){
-
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                                        new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_genius_logo)
+                        .build(),
+                RC_SIGN_IN);
     }
 
     private void showNoConnexionError() {
@@ -72,5 +118,29 @@ public class BootstrapActivity extends AppCompatActivity {
         mytoast.setView(view);
         mytoast.setDuration(Toast.LENGTH_LONG);
         mytoast.show();
+    }
+
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+                startNextActivity();
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
     }
 }
